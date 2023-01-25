cc485bfc088915434b909c1a51643c61a3adeb5c
David Jhonson <alx.zolotarov@gmail.com>
2023-01-25 19:44:23 +0000

    Update: configured pylsp & added ansible snippets

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -7,3 +7,22 @@ set.showtabline=2
 
 set.expandtab = true
 set.number = true 
+
+-- Snippets
+require("luasnip.loaders.from_snipmate").lazy_load({
+  paths = { "./snippets" }
+})
+
+local lsp = require('lsp-zero')
+lsp.preset('recommended')
+lsp.setup()
+
+-- Specific language server configuration
+
+require'lspconfig'.pylsp.setup{
+  settings = {
+    pylsp = {
+      plugins = false 
+    }
+  }
+}
