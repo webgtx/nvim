97872cbb538a290e527b08f6e5787dced47556c6
David Jhonson <alx.zolotarov@gmail.com>
2023-08-31 05:57:25 +0100

    Update: bash script refactoring

--- setup.sh
+++ setup.sh
@@ -10,46 +10,49 @@ set_colorscheme() {
   fi
 }
 
-if [[ $1 = "--theme" ]] || [[ $1 = "-t" ]]; then
-  printf "1) Onedark\n2) Oh-Lucy\n3) Adwaita\n4) Tokyodark\n5) Github\n"
-  read -p "[ Choose colorscheme from list ]: " colorscheme
+echo "[ Setup script started ]"
+echo "[ Installing neovim directory ... ]"
+cp -r ./nvim $HOME/.config
 
-  case "$colorscheme" in
-    1)  
-      set_colorscheme onedark 
+for arg in $@; do
+  case "$arg" in
+    "--help"|"-h")
+      cat .data/help.dat
     ;;
-    2)
-      set_colorscheme oh-lucy
+    "--theme"|"-t")
+      printf "1) Onedark\n2) Oh-Lucy\n3) Adwaita\n4) Tokyodark\n5) Github\n0) Default (transparent)\n"
+      read -p "[ Choose colorscheme from list ]: " colorscheme
+
+      case "$colorscheme" in
+        1)  
+          set_colorscheme onedark 
+        ;;
+        2)
+          set_colorscheme oh-lucy
+        ;;
+        3)
+          set_colorscheme adwaita
+        ;;
+        4)
+          set_colorscheme tokyodark
+        ;;
+        5)
+          set_colorscheme github_*
+        ;;
+        0)
+          cat /dev/null > $HOME/.config/nvim/lua/colorscheme.lua  
+        ;;
+        *)
+          echo "Undefined colorscheme, try something else"
+        ;;
+      esac
     ;;
-    3)
-      set_colorscheme adwaita
-    ;;
-    4)
-      set_colorscheme tokyodark
-    ;;
-    5)
-      set_colorscheme github_*
+    "--clear"|"-c")
+      rm -rf $HOME/.config/nvim 
     ;;
     *)
-      echo "Undefined colorscheme, try something else"
+      cat .data/help.dat
     ;;
   esac
-
-elif [[ $1 = "--packer" ]] || [[ $1 = "-p" ]]; then
-  git clone --depth 1 https://github.com/wbthomason/packer.nvim \
-   ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-
-elif [[ $1 == "--install" ]] || [[ $1 == "-i" ]]; then 
-  echo "Setting up colorscheme..."
-  if [[ -f "~/.local/share/nvim/site/pack/packer/start/packer.nvim" ]]; then
-    . ./setup.sh -p & 
-  else
-    echo "packer has already been installed"
-  fi
-
-  cp -r nvim $HOME/.config
-
-elif [[ $1 == "" ]]; then
-  cat ./.data/help.dat 
-fi
+done
 
